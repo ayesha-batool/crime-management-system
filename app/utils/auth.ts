5cb360959836851ebf4371dfe4a17efddcb7ba93
Asiya1330 <asiya.batool987@gmail.com>
2024-05-09 00:19:29 +0500

    fixed localstorage

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -3,8 +3,14 @@
 export const getLoggedInUser = (): string | null => {
     return localStorage.getItem('loggedInUser');
   };
-  export const getLoggedInUserEmail = (): string | null => {
+// utils/auth.ts
+export const getLoggedInUserEmail = (): string | null => {
+    if (typeof window === 'undefined') {
+      // Check if running on the server-side
+      return null; // Return null if localStorage is not available
+    }
     const userJson = localStorage.getItem('loggedInUser');
     const user = userJson ? JSON.parse(userJson) : null;
     return user ? user.email : null;
   };
+  
